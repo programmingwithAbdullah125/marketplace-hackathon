7b1da69623d4360a9eb31f13d893d32a299c9a15
Zonikhan <150387908+programmingwithAbdullah125@users.noreply.github.com>
2025-02-08 01:18:40 +0500

    update mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,25 @@
-// nect.config.mjs to next.config.ts
+// // nect.config.mjs to next.config.ts
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
+//       },
+// };
+
+// export default nextConfig;
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
+    ],
+  },
 };
 
 export default nextConfig;
